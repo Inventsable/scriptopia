c449f2ee53f9cc97955958bc3d394d55c7a61547
Tom Scharstein <trscharstein@gmail.com>
2019-12-04 21:32:59 -0700

    Initial build complete, 100% support, readme

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # scriptopia
 
-Generate Typescript support for any compatible Adobe app with a single terminal command
+Generate Typescript support for any compatible Adobe app with a single terminal command:
+
+![screen capture]()
+
+## Installation
+
+```bash
+# Recommended global so you can use this any time, in any folder
+npm install -g scriptopia
+```
+
+## Usage
+
+Scriptopia will download reliable typescript definitions to your project (but skip if already present), then generate a `tsconfig.json` and `.ts` file in your current working directory.
+
+Using the command with no arguments will result in a CLI prompt for which application to script for, and what the name of the file should be:
+
+```bash
+scriptopia
+```
+
+You can skip this prompt by adding them as arguments instead. Any of the below are valid:
+
+```bash
+scriptopia ai test.js
+scriptopia ilst file.ts
+scriptopia illustrator myScript.jsx
+```
+
+## App aliases (case insensitive):
+
+- Illustrator - `ai`, `ilst`, or `illustrator`
+- After Effects - `ae`, `aeft`, `aftereffects`, or `afterfx`
+- Photoshop - `ps`, `phxs`, or `photoshop`
+- InDesign - `id`, `idsn`, or `indesign`
+- Premeire Pro - `pp`, `ppro`, `premiere`, or `premierepro`
+- Audition - `au`, `audt`, or `audition`
--- bin/index.js
+++ bin/index.js
@@ -2,40 +2,71 @@
 
 const fs = require("fs");
 const path = require("path");
-let helpme = false;
-let cwd = path.dirname(fs.realpathSync(__filename));
+const shell = require("shelljs");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const ora = require("ora");
+shell.config.silent = true;
+
+let ORA_SPINNER = {
+  interval: 80,
+  frames: [
+    "   ⠋",
+    "   ⠙",
+    "   ⠚",
+    "   ⠞",
+    "   ⠖",
+    "   ⠦",
+    "   ⠴",
+    "   ⠲",
+    "   ⠳",
+    "   ⠓"
+  ]
+};
+
+let cwd = path.resolve("./");
 let taskHotkeys = /win|linux/i.test(process.platform)
   ? "CONTROL + SHIFT + B"
   : "COMMAND + SHIFT + B";
 const config = {
   alwaysAsk: false,
   useLatest: true,
-  quiet: false
+  quiet: false,
+  here: false,
+  needsInstall: false,
+  fileName: "host",
+  customName: false
 };
 const apps = {
-  AEFT: {
-    aliases: ["aeft", "ae", "after effects", "afterfx"],
-    versions: ["2018"]
-  },
-  ILST: {
+  Illustrator: {
+    name: "Illustrator",
     aliases: ["ilst", "ai", "illustrator"],
     versions: ["2015.3", "2017", "2018", "2019", "2020"]
   },
-  PHXS: {
+  AfterEffects: {
+    name: "AfterEffects",
+    aliases: ["aeft", "ae", "after effects", "afterfx"],
+    versions: ["2018"]
+  },
+  Photoshop: {
+    name: "Photoshop",
     aliases: ["phxs", "ps", "photoshop"],
     versions: ["2015.3"]
   },
-  AUDT: {
-    aliases: ["audt", "au", "audition"],
-    versions: ["2015.2", "2017", "2018"]
+  InDesign: {
+    name: "InDesign",
+    aliases: ["idsn", "id", "indesign", "in design"],
+    versions: ["2015.3", "2018", "2019"]
   },
-  PPRO: {
+  Premiere: {
+    name: "Premiere",
     aliases: ["ppro", "pp", "premiere", "premiere pro"],
     versions: ["2018", "2019"]
   },
-  IDSN: {
-    aliases: ["idsn", "id", "indesign", "in design"],
-    versions: ["2015.3", "2018", "2019"]
+  Audition: {
+    name: "Audition",
+    aliases: ["audt", "au", "audition"],
+    versions: ["2015.2", "2017", "2018"]
   }
 };
 
@@ -52,20 +83,52 @@ let tsconfig = {
 };
 
 async function init() {
-  const args = process.argv;
-  console.log(cwd);
+  let app,
+    args = process.argv;
   checkForConfigArgs(args);
-  if (helpme) return showHelp();
-  let app;
-  if (args.length > 2) app = await findApp(args);
-  app = app ? app : await inquireAboutApp();
-  if (!config.quiet) console.log(app);
+  let fileCheck = args.filter(arg => {
+    return /\w*\.(t|j)s(x?)/.test(arg);
+  });
+  if (fileCheck.length)
+    config.fileName = fileCheck[0].replace(/\.(t|j)s(x?)/, "");
+  if (args.includes("help")) showHelp();
+  else {
+    let targPath = !config.here ? await findNearestParentPackage(cwd) : cwd;
+    config.needsInstall = await checkPackageForInstall(targPath);
+    if (args.length > 2) app = await findApp(args);
+    app = app ? app : await inquireAboutApp();
+
+    generateTSConfig(app, targPath);
+    writeDefaultFiles();
+    if (config.needsInstall) installTypes(targPath, app);
+    else showEndMessage(targPath, app);
+  }
 }
 
 async function inquireAboutApp() {
-  //
-  console.log(process.argv);
-  return true;
+  console.log("");
+  console.log(`Welcome! Let's get started.`);
+  console.log("");
+  let answers = await inquirer.prompt([
+    {
+      type: "list",
+      name: "app",
+      message: `Which app are you scripting for?`,
+      choices: Object.keys(apps).map(item => {
+        return {
+          name: item !== "AfterEffects" ? item : "After Effects",
+          value: item
+        };
+      })
+    },
+    {
+      type: "input",
+      name: "name",
+      message: `What should the name of the script file be?`
+    }
+  ]);
+  config.fileName = answers.name.replace(" ", "-").replace(/\.(j|t)s(x?)/, "");
+  return apps[answers.app];
 }
 
 async function findApp(args) {
@@ -82,6 +145,94 @@ async function findApp(args) {
   return result ? result : await inquireAboutApp();
 }
 
+function showHelp() {
+  console.log("");
+  console.log("Welcome!");
+  console.log("");
+  console.log(
+    `This package is best used when installed globally, but relies on a local package.json to install type definitions. If you haven't already, you should run ${chalk.yellow(
+      "npm init -y"
+    )} somewhere in your project.`
+  );
+  console.log("");
+  console.log(
+    `You can then run ${chalk.yellow(
+      "scriptopia"
+    )} at any time to get a prompt about the app and name of your resulting file.`
+  );
+  console.log("");
+  console.log(
+    `If you want to skip the prompts, you can optionally add the app name or file name after the command, like so:`
+  );
+  console.log("");
+  console.log(`  - ${chalk.yellow("scriptopia ai test.jsx")}`);
+  console.log(`  - ${chalk.yellow("scriptopia ilst myscript.ts")}`);
+  console.log(`  - ${chalk.yellow("scriptopia illustrator somescript.js")}`);
+  console.log("");
+  console.log(
+    `You can see more details at ${chalk.blue(
+      `https://github.com/Inventsable/scriptopia`
+    )}`
+  );
+  console.log("");
+}
+
+function showEndMessage(thispath, app) {
+  let trail = differenceFromRoot(thispath);
+  let root = thispath.match(/[^\\|\/]*$/)[0];
+  let relpath = `${root}/${trail.travelDown}`;
+  relpath = relpath.replace(/\\/gm, "/");
+  console.log("");
+  console.log(`✔  We're all done!`);
+
+  console.log("");
+  console.log(
+    `Now hit ${chalk.yellow(`${taskHotkeys}`)} and select ${chalk.bgBlue.black(
+      ` tsc: watch - ${relpath}/tsconfig.json `
+    )}`
+  );
+  console.log("");
+  console.log(
+    `You should now use ${chalk.green(
+      `${config.fileName}.ts`
+    )} to write code, but ${chalk.green(`${config.fileName}.jsx`)} to run in ${
+      app.name
+    }.`
+  );
+  console.log("");
+}
+
+function writeDefaultFiles() {
+  fs.writeFileSync(
+    `${path.resolve(cwd)}/${config.fileName.replace(/\..*/, "")}.ts`,
+    `// Welcome to the world of Typescript!
+// Be sure to run 'tsc: watch - [path to tsconfig]', then try writing 'app.' below:`
+  );
+}
+
+async function readDir(thispath) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(path.resolve(thispath), { encoding: "utf-8" }, (err, files) => {
+      if (err) reject(err);
+      resolve(files);
+    });
+  });
+}
+
+function generateTSConfig(app, thispath) {
+  let version = config.useLatest
+    ? app.versions[app.versions.length - 1]
+    : app.versions[0];
+  tsconfig.compilerOptions.outFile = `./${config.fileName}.jsx`;
+  tsconfig.compilerOptions.types = [`types-for-adobe/${app.name}/${version}`];
+  tsconfig.files = [`${config.fileName}.ts`];
+  fs.writeFileSync(
+    path.resolve(`${cwd}/tsconfig.json`),
+    JSON.stringify(tsconfig)
+  );
+}
+
+// Redundant, not using enough config args to justify this.
 function checkForConfigArgs(args) {
   let valids = args.filter(arg => {
     return /^\-[a-z]*$/i.test(arg);
@@ -90,19 +241,86 @@ function checkForConfigArgs(args) {
   let possibles = {
     alwaysAsk: ["ask", "a"],
     useLatest: ["latest", "l"],
-    quiet: ["quiet", "q"]
+    quiet: ["quiet", "q"],
+    here: ["here", "h"]
   };
   valids.forEach(valid => {
     Object.keys(possibles).forEach(key => {
       possibles[key].forEach(term => {
-        if (/\-h|\-help/) helpme = true;
-        config[key] = new RegExp(`^\-${term}$`, "i").test(valid);
+        config[key] = !config[key]
+          ? new RegExp(`^\-${term}$`, "i").test(valid)
+          : true;
       });
     });
   });
 }
-function showHelp() {
-  console.log("WELCOME!");
+
+// Redundant, not needed
+async function findNearestParentPackage(thispath) {
+  let children = await readDir(thispath);
+  if (!children.length || !children.includes("package.json"))
+    return await findNearestParentPackage(thispath.replace(/\/|\\\w*$/, ""));
+  else if (children.includes("package.json")) return thispath;
+}
+
+// Redundant since npm install automatically handles parenting chains
+async function installTypes(thispath, app) {
+  // let paths = differenceFromRoot(thispath);
+  // shell.cd(paths.travelUp);
+  let spinner = ora({
+    text: `Installing types to ${
+      thispath.match(/[^\\|\/]*$/)[0]
+    }... (This may take a while)`,
+    spinner: ORA_SPINNER
+  }).start();
+  shell.exec("npm install ten-A/types-for-adobe --save-dev", () => {
+    console.log("");
+    spinner.stopAndPersist({
+      symbol: "",
+      text: `${chalk.black.bgBlue(` ✔ SUCCESSFULLY INSTALLED `)}`
+    });
+    showEndMessage(thispath, app);
+  });
+}
+
+async function checkPackageForInstall(thispath) {
+  let files = await readDir(thispath);
+  let found = false;
+  if (files.length && files.includes("package.json")) {
+    let manifest = JSON.parse(
+      fs.readFileSync(path.resolve(`${thispath}/package.json`), {
+        encoding: "utf-8"
+      })
+    );
+    let keys = [];
+    if (manifest.dependencies)
+      keys = [].concat(keys, Object.keys(manifest.dependencies));
+    if (manifest.devDependencies)
+      keys = [].concat(keys, Object.keys(manifest.devDependencies));
+    if (!keys.length) return true;
+    keys.forEach(dep => {
+      if (dep == "types-for-adobe") found = true;
+    });
+  }
+  return !found;
+}
+
+// Redundant, npm install automatically handles parent chaining
+function differenceFromRoot(thispath) {
+  let remainder = cwd.replace(thispath, "");
+  if (!remainder.length) return { travelUp: "./", travelDown: "./" };
+  else {
+    remainder = remainder.replace(/(^\\|\/)|(\\|\/)$/, "");
+    let depths = remainder.split(/\\|\//);
+    return {
+      travelUp: depths
+        .map(d => {
+          return "..";
+        })
+        .join("/"),
+      travelDown: remainder
+    };
+  }
 }
 
 init();
